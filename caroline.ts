a56847a8bf9703987058adef47c812ebf7449f08
notangelmario <savin@angelmario.eu>
2023-01-27 01:52:31 +0200

    Added Caroline

---
+++ caroline.ts
@@ -0,0 +1,9 @@
+import { main } from "./src/main.ts";
+
+
+main({
+	name: "caroline",
+	model: "text-davinci-003",
+	instructions: "Your name is Caroline. Pretend you are a sentient AI assistant.",
+	publicName: "Caroline",
+});
